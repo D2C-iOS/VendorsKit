7b8668737723d4989664761dafd05b88b41443a5
cezres <cezr@sina.com>
2016-10-08 16:15:52 +0800

    初始化项目

---
+++ VendorsKit/VendorsKit.h
@@ -0,0 +1,19 @@
+//
+//  VendorsKit.h
+//  VendorsKit
+//
+//  Created by 翟泉 on 2016/10/8.
+//  Copyright © 2016年 云之彼端. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VendorsKit.
+FOUNDATION_EXPORT double VendorsKitVersionNumber;
+
+//! Project version string for VendorsKit.
+FOUNDATION_EXPORT const unsigned char VendorsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VendorsKit/PublicHeader.h>
+
+
